d0734b91ab3517224d2437ff833e70d9486ead5b
helenafa <helenafa@stud.ntnu.no>
2020-10-05 17:05:21 +0100

    Added JS file

---
+++ petter-script.js
@@ -0,0 +1,13 @@
+
+
+var oppfinnelser = [
+'Fantasimaskin',
+'Automatisk oppfinner',
+'Automatisk peanøttspreder',
+'Bjelle for fiskefangst',
+'Brødrister med månesprett',
+'Brødskivesmører'
+
+];
+
+console.log(oppfinnelser[0]);
